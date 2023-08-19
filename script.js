c57d8f8e50ee6a7aeef10d286d16c15300b4e4be
Allison Serrano <allisonserrano171@yahoo.com>
2023-08-19 17:02:58 -0400

    added neccessary files and style to web app

---
+++ script.js
@@ -0,0 +1,4 @@
+//For the base url we want to use the parameter &limit={limit} and q={city name},{state code},{country code}
+// and units=imperial to get fahrenheit for temperture
+//My API key d45477c23f64263ae329c1cedb3ece85
+
