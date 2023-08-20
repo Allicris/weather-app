21131f1b238a75932758753aff9b396e440160ba
Allison Serrano <allisonserrano171@yahoo.com>
2023-08-20 19:43:09 -0400

    changes

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -10,15 +10,16 @@ const weatherIcon = document.querySelector(".weather-icon");
 const savedSearches = [];
 
 // var today = moment();
-// $(".date").text(today.format("MMM DD, YYYY"));
+// let dateFormat = moment().format('D-MM-YYYY');
+// console.log(dateFormat1); // 23-08-2022
 
-async function currentWeather(city){
+async function currentWeather(city) {
   const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial";
 
   const response = await fetch(apiUrl);
   var data = await response.json();
 
-  if(response.status == 404) {
+  if (response.status == 404) {
     document.querySelector(".search p").style.display = "block";
     document.querySelector(".weather").style.display = "none";
   }
@@ -32,27 +33,40 @@ async function currentWeather(city){
   document.querySelector('.wind').innerHTML = Math.round(data.wind.speed) + " " + "mph/hr";
   document.querySelector('.humidity').innerHTML = "Humidity" + " " + Math.round(data.main.humidity);
 
-  if(data.weather[0].main == 'Clouds') {
+  if (data.weather[0].main == 'Clouds') {
     weatherIcon.src = "assets/images/cloudy.png";
   } else if (data.weather[0].main == 'Clear') {
     weatherIcon.src = "assets/images/sunny.png";
   } else if (data.weather[0].main == 'Rain') {
     weatherIcon.src = "assets/images/rainy.png";
-  } else if (data.weather[0].main == 'Snow'){
+  } else if (data.weather[0].main == 'Snow') {
     weatherIcon.src = "assets/images/snow.png";
   }
 }
 
-async function fiveDay(city) {
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial";
-}
+async function getFiveDay() {
+  const searchCity = document.querySelector(".search input");
+  //api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key})
+  const fiveDayUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + searchCity.value + "&appid=" + apiKey;
+  const response = await fetch(fiveDayUrl);
+  const data = await response.json();
+  
+    for (i = 0; i < 5; i++) {
+      document.querySelector('.tempy').innerHTML = Math.round(data.list[i].main.temp) + " " + "°";
+    }
+    for (i = 0; i < 5; i++) {
+      document.querySelector('.windy').innerHTML = Math.round(data.wind.speed) + " " + "mph/hr";
+    }
+    for (i = 0; i < 5; i++) {
+      document.querySelector('.humid').innerHTML = "Humidity" + " " + Math.round(data.list[i].main.humidity);
+    }
+  }
 
 searchBtn.addEventListener("click", () => {
-currentWeather(searchCity.value);
-document.querySelector(".search p").style.display = "none";
-document.querySelector(".weather").style.display = "block";
+  currentWeather(searchCity.value);
+  document.querySelector(".search p").style.display = "none";
+  document.querySelector(".weather").style.display = "block";
 });
 
-document.getElementById
-
+getFiveDay();
 currentWeather();
