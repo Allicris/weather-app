41bcb6918fd1c0736a7428df565cb2f23a00a2c5
Allison Serrano <allisonserrano171@yahoo.com>
2023-08-20 11:56:34 -0400

    changed icons depending on the weather

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,14 +1,44 @@
 //For the base url we want to use the parameter &limit={limit} and q={city name},{state code},{country code}
-// and units=imperial to get fahrenheit for temperture
-//My API key d45477c23f64263ae329c1cedb3ece85
 
 const apiKey = "d45477c23f64263ae329c1cedb3ece85";
-const currentApiUrl = "https://api.openweathermap.org/data/2.5/weather?q=lodi&units=imperial";
-const fiveDayApi = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}units=imperial";
+const currentApiUrl = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=";
+//const fiveDayApi = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}units=imperial";
+const searchCity = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
 
-async function currentWeather(){
-  const response = await fetch(currentApiUrl + `appid=${apiKey}`);
+async function currentWeather(city){
+  const response = await fetch(currentApiUrl + city + `&appid=${apiKey}`);
   var data = await response.json();
 
+  if(response.status == 404) {
+    document.querySelector.apply(".error").style.display = "block"
+  }
+
   console.log(data);
+
+  // document.querySelector('.date').innerHTML = "";
+  document.querySelector('.city').innerHTML = data.name;
+  //This needs to be rounded up.
+  document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + " " + "°";
+  document.querySelector('.wind').innerHTML = Math.round(data.wind.speed) + " " + "mph/hr";
+  document.querySelector('.humidity').innerHTML = Math.round(data.main.humidity);
+
+  if(data.weather[0].main == 'Clouds') {
+    weatherIcon.src = "assets/images/cloudy.png";
+  } else if (data.weather[0].main == 'Clear') {
+    weatherIcon.src = "assets/images/sunny.png";
+  } else if (data.weather[0].main == 'Rain') {
+    weatherIcon.src = "assets/images/rainy.png";
+  } else if (data.weather[0].main == 'Snow'){
+    weatherIcon.src = "assets/images/snow.png";
+  }
+
 }
+
+searchBtn.addEventListener("click", () => {
+
+currentWeather(searchCity.value);
+});
+
+currentWeather();
